235e919b9568956d114c4c35d1d45d9207242635
Juan <bravo43juancho@gmail.com>
2024-10-13 19:03:01 -0300

    Add base stuctural css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { AppComponent } from './app.component';
 import { EducationPageComponent } from './sections/education-page/education-page.component';
 import { ProyectsPageComponent } from './sections/proyects-page/proyects-page.component';
 import { HomePageComponent } from './sections/home-page/home-page.component';
+import { AboutPageComponent } from './sections/about-page/about-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'education', component: EducationPageComponent },
     { path: 'proyects', component: ProyectsPageComponent },
-    { path: 'about', component: ProyectsPageComponent },
+    { path: 'about', component: AboutPageComponent },
 ];
