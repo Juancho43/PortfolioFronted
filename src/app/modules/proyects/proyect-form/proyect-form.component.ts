bda345319cdc6638b989ca9e07d379ef20ed56c9
Juan <bravo43juancho@gmail.com>
2024-12-16 01:40:47 -0300

    Add tag manager on proyectForm

--- src/app/modules/proyects/proyect-form/proyect-form.component.ts
+++ src/app/modules/proyects/proyect-form/proyect-form.component.ts
@@ -1,19 +1,27 @@
 import {Component, inject} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {ProyectsService} from "../../../core/services/proyects.service";
 import {Proyecto} from "../../../core/interfaces/Proyecto";
+import {TagComponent} from "../../tags/tag/tag.component";
+import {TagsService} from "../../../core/services/tags.service";
+import {Tag} from "../../../core/interfaces/Tag";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-proyect-form',
   standalone: true,
-    imports: [
-        ReactiveFormsModule
-    ],
+  imports: [
+    ReactiveFormsModule,
+    TagComponent,
+    JsonPipe
+  ],
   templateUrl: './proyect-form.component.html',
   styleUrls: ['./proyect-form.component.css','../../../core/styles/forms.css']
 })
 export class ProyectFormComponent {
   private proyectsService = inject(ProyectsService);
+  private tagsService = inject(TagsService);
+  tags : Tag[] = [];
   edit : boolean = false;
   currentProyect : Proyecto = {id:0, nombre:"", descripcion: "",  fechaCreacion: new Date(), tags: []};
   ProyectForm : FormGroup = new FormGroup({
@@ -21,14 +29,19 @@ export class ProyectFormComponent {
     nombre : new FormControl('', [Validators.required]),
     descripcion: new FormControl('', [Validators.required]),
     fechaCreacion: new FormControl(new Date(), [Validators.required]),
-    tags: new FormControl([], Validators.required)
   })
 
   ngOnInit () {
-
+  this.getData();
   }
 
-
+  getData(){
+    this.tagsService.getTags().subscribe({
+      next : (x) => {
+        this.tags = x.tagDTOList;
+      }
+    })
+  }
   onSubmit(){
     this.mapperProyecto();
     if(!this.edit){
@@ -41,4 +54,13 @@ export class ProyectFormComponent {
     this.currentProyect.descripcion = this.ProyectForm.get("descripcion")?.value;
     this.currentProyect.fechaCreacion = this.ProyectForm.get("fechaCreacion")?.value;
   }
+  addTag(tag : Tag){
+    if(!this.currentProyect.tags.find(p => p == tag)){
+      this.currentProyect.tags.push(tag);
+    }
+  }
+  removeTag(tag : Tag){
+    this.currentProyect.tags = this.currentProyect.tags.filter(p => p != tag);
+  }
+
 }
--- src/app/modules/proyects/proyect/proyect.component.ts
+++ src/app/modules/proyects/proyect/proyect.component.ts
@@ -1,11 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {Proyecto} from "../../../core/interfaces/Proyecto";
 import {CommonModule} from "@angular/common";
+import {TagComponent} from "../../tags/tag/tag.component";
 
 @Component({
   selector: 'app-proyect',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TagComponent],
   templateUrl: './proyect.component.html',
   styleUrl: './proyect.component.css'
 })
