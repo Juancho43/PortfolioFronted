c56ef430fad224985454c44c9c241f058554cad8
Juan <bravo43juancho@gmail.com>
2025-01-05 02:00:54 -0300

    Education reverse style

--- src/app/modules/education/education/education.component.ts
+++ src/app/modules/education/education/education.component.ts
@@ -1,21 +1,20 @@
 import {Component, inject, Input} from '@angular/core';
-import {ProyectDaoService} from '../../../core/DAO/proyect-dao.service';
-import {Proyecto} from '../../../core/interfaces/Proyecto';
 import {Education} from '../../../core/interfaces/Education';
 import {EducationDaoService} from '../../../core/DAO/education-dao.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
   private dao = inject(EducationDaoService);
   @Input() education : Education = this.dao.getEmptyEducation();
-
-  ngOnInit(){
+  @Input() reverse : boolean = false;
+   ngOnInit(){
 
 
   }
