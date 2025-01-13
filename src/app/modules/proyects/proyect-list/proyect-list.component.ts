da6bbce3f5866df26cccbf2eb6a3c4d272361fdf
Juan <bravo43juancho@gmail.com>
2025-01-12 23:56:17 -0300

    Contact info and photo

--- src/app/modules/proyects/proyect-list/proyect-list.component.ts
+++ src/app/modules/proyects/proyect-list/proyect-list.component.ts
@@ -1,25 +1,25 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {ProyectsService} from "../../../core/services/proyects.service";
-import {CommonModule} from "@angular/common";
-import {ProyectComponent} from "../proyect/proyect.component";
-import {Proyecto} from "../../../core/interfaces/Proyecto";
+import { Component, inject, OnInit } from '@angular/core';
+import { ProyectsService } from '../../../core/services/proyects.service';
+import { CommonModule } from '@angular/common';
+import { ProyectComponent } from '../proyect/proyect.component';
+import { Proyecto } from '../../../core/interfaces/Proyecto';
 
-import {ProyectCardComponent} from "../proyect-card/proyect-card.component";
-import {DialogService} from "../../../core/utils/dialog.service";
-import {ProyectDaoService} from "../../../core/DAO/proyect-dao.service";
+import { ProyectCardComponent } from '../proyect-card/proyect-card.component';
+import { DialogService } from '../../../core/utils/dialog.service';
+import { ProyectDaoService } from '../../../core/DAO/proyect-dao.service';
 
 @Component({
   selector: 'app-proyect-list',
   standalone: true,
   imports: [CommonModule, ProyectCardComponent],
   templateUrl: './proyect-list.component.html',
-  styleUrl: './proyect-list.component.css'
+  styleUrl: './proyect-list.component.css',
 })
 export class ProyectListComponent implements OnInit {
   private proyectsService = inject(ProyectsService);
   private proyectsDAO = inject(ProyectDaoService);
   private dialog = inject(DialogService);
-  proyects : Proyecto[] = [];
+  proyects: Proyecto[] = [];
 
   ngOnInit() {
     this.getDataDao();
@@ -30,33 +30,23 @@ export class ProyectListComponent implements OnInit {
     this.proyectsDAO.setProyectos([]);
   }
 
-  getDataDao(){
-    this.proyectsDAO.getProyectos().subscribe(res=>{
+  getDataDao() {
+    this.proyectsDAO.getProyectos().subscribe((res) => {
       this.proyects = res;
-      console.log(this.proyects)
-    })
+    });
   }
 
-  getData(){
-
-
+  getData() {
     this.proyectsService.getProyects().subscribe({
-
-      next : (x) => {
-        this.proyects = x.Proyect;
-        console.log(this.proyects)
-
-      }
-    })
-
+      next: (x) => {
+        this.proyects = x.Projects;
+      },
+    });
   }
 
-
-  open(item : Proyecto){
+  open(item: Proyecto) {
     this.proyectsDAO.setProyecto(item);
 
-    this.dialog.openModal<ProyectComponent,Proyecto>(
-      ProyectComponent
-    );
+    this.dialog.openModal<ProyectComponent, Proyecto>(ProyectComponent);
   }
 }
