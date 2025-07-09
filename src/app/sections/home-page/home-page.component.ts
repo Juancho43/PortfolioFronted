76e5447cb95afb0850237aa0d42383b200eb413b
Juan Bravo <bravo43juancho@gmail.com>
2025-07-09 16:19:08 -0300

    feat: implement meta tag management in HomePageComponent for improved SEO

--- src/app/sections/home-page/home-page.component.ts
+++ src/app/sections/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { MetaTagsService } from '@services/utils/meta-tags.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
-export default class HomePageComponent {
+export default class HomePageComponent implements OnInit, OnDestroy {
+  private metaTagService = inject(MetaTagsService);
 
+  ngOnInit(): void {
+    this.metaTagService.addTitle('Acerca de: Bravo, Juan Alé');
+    this.metaTagService.addDescriptionMetaTag('Porfolio de Juan Alé, programador full stack. Descubre sus proyectos, habilidades y trayectoria profesional.')
+
+  }
+
+  ngOnDestroy(): void {
+    this.metaTagService.addTitle('Bravo, Juan Alé');
+    this.metaTagService.removeAllMetaTags();
+  }
 }
